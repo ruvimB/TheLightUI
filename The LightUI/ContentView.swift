61a4d80d4a5c5a9be0599af6c1c4a14fc32763ba
Ruvim Burlakov <ruvim.burlakov@yandex.ru>
2021-03-06 12:03:43 +0300

    Initial Commit

---
+++ The LightUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The LightUI
+//
+//  Created by Ruvim on 06.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The LightUI/The_LightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_LightUIApp.swift
+//  The LightUI
+//
+//  Created by Ruvim on 06.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct The_LightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
